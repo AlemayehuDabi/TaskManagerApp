ff81d3f563f101a20fb5a9dce3da271242260359
Alemayehu Dabi <alemayehudabi606@gmail.com>
2024-07-14 12:46:22 +0300

    port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const port = 8000;
+const port = process.env.PORT || 8000;
 const userRouter = require("./Routes/userRouter");
 
 const taskRouter = require("./Routes/taskRouter");
