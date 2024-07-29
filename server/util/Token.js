762648dc0e1c5ba329cbc896eaa28cb5cfd5dcc1
AlemayehuDabi <alemayehudabi606@gmail.com>
2024-07-29 13:26:27 +0300

    Update Token.js

--- server/util/Token.js
+++ server/util/Token.js
@@ -6,7 +6,6 @@ const createToken = (_id, date) => {
   });
 };
 
-//mongodb+srv://Alemayehu:@kokobe1214sr71blackbird#13@taskmanagerapp.dk8b8lj.mongodb.net/?retryWrites=true&w=majority&appName=taskManagerApp
 
 module.exports = {
   createToken,
